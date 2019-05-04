5464d0e35e0fe9a80e45e74c6d605a8f2db2f4fc
swathi5896 <swathi5896.r@gmail.com>
2019-05-04 18:34:26 +0530

    updated with pymongo

--- bse_fleet.py
+++ bse_fleet.py
@@ -1,69 +1,49 @@
 import json
 
-import pymysql.cursors
+import pymysql
+import pymongo
 from flask import Flask, render_template, request
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/authorize',methods = ['POST', 'GET'])
-def login():
-   if request.method == 'POST':
-      user = request.form['nm']
-      password = request.form['ps']
-      #print user,password
-      return 'ok'
+@app.route('/authorize', methods=['POST', 'GET'])
+def authorize():
+    if request.method == 'POST':
+        b = request.get_json()
+        u1 = b['user']
+        p = b['password']
+        print(b, u1, p)
+    dba = pymysql.connect(host='localhost', user='root',password='Swathi_123', db='admin')
+    mycursor = dba.cursor()
+    mycursor.execute("SELECT * FROM customer_details WHERE customer_id=%s", [u1])
+    result = mycursor.fetchone()
+    print(result)
+    if result:
+        print("yes")
+        if result[2] == p:
+            val = json.dumps({'status': True, 'reason': 'successful'})
+        else:
+            val = json.dumps({'status': False, 'reason': 'Incorrect Password'})
+    else:
+        print("no")
+        val = json.dumps({'status': False, 'reason': 'Incorrect Username'})
+    dba.close()
+    return val
 
-@app.route('/authorize/<user>/<password>')
-def authorize(user,password):
-   p=str(password)
-   u1=str(user)
-   dba = connection = pymysql.connect(host='localhost',user='root', password='Swathi_123',db='admin')
-   mycursor = dba.cursor()
-
-   values = mycursor.execute("SELECT customer_id FROM customer_details")
-   result =list(mycursor.fetchall())
-   l= len(result)
-   user_list=[]
-   for i in range (l):
-      u2=str((result[i])[0])
-      user_list.append(u2)
-   if u1 in user_list:
-      values1 = mycursor.execute("SELECT password FROM customer_details WHERE customer_id=%s",[u1])
-      result1 =mycursor.fetchone()
-      r=result1[0]
-      if r==p:
-         val=json.dumps({'status':True})
-      else:
-         val=json.dumps({'status':False})
-   else:
-      val=json.dumps({'status':False})
-   dba.close()
-   return val
-
-
-@app.route('/location',methods = ['POST', 'GET'])
+@app.route('/location', methods=['POST', 'GET'])
 def locate():
-   if request.method == 'GET':
-      customer_id = request.args.get('c_id')
-      app_id = request.args.get('a_id')
-      dba = connection = pymysql.connect(host='localhost',user='root', password='Swathi_123',db=customer_id)
-      mycursor = dba.cursor()
-      values = mycursor.execute("SELECT latitude, longitude FROM tracking_details WHERE app_id=%s ORDER BY id DESC LIMIT 1",[app_id])
-      result =mycursor.fetchall()
-      l1={'latitude':(result[0])[0],'longitude':(result[0])[1]}
-      l2=json.dumps(l1)
-      dba.close()
-   return l2
-
-#to send json data
-"""@app.route('/loca/')
-def lo():
-    k={'tyt':'swa','lus':'sha'}
-    l=json.dumps(k)
-    return l"""
-      
+    if request.method == 'GET':
+        customer_id = request.args.get('c_id')
+        app_id = request.args.get('a_id')
+        myclient=pymongo.MongoClient("mongodb://localhost:27017/")
+        mydb=myclient[customer_id]
+        mycol=mydb["tracking_details"]
+        location=mycol.find_one({'app_id':app_id},{"_id":False})
+        myclient.close()
+        l=json.dumps(location)
+    return l
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0',debug = True)
+    app.run(host='0.0.0.0', debug=True)
