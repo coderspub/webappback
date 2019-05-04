c8868c044b650c4caa3a6636f9af8a1a429d001c
swathi5896 <swathi5896.r@gmail.com>
2019-05-04 14:14:37 +0530

    updated on 03-05-2019

--- bse_fleet.py
+++ bse_fleet.py
@@ -1,9 +1,11 @@
+import json
+
+import pymysql.cursors
 from flask import Flask, render_template, request
 from flask_cors import CORS, cross_origin
+
 app = Flask(__name__)
 CORS(app)
-import MySQLdb
-import json
 
 @app.route('/authorize',methods = ['POST', 'GET'])
 def login():
@@ -17,7 +19,7 @@ def login():
 def authorize(user,password):
    p=str(password)
    u1=str(user)
-   dba = MySQLdb.connect(host="localhost", user="root", passwd="Swathi_123", db="admin")
+   dba = connection = pymysql.connect(host='localhost',user='root', password='Swathi_123',db='admin')
    mycursor = dba.cursor()
 
    values = mycursor.execute("SELECT customer_id FROM customer_details")
@@ -25,33 +27,28 @@ def authorize(user,password):
    l= len(result)
    user_list=[]
    for i in range (l):
-	u2=str((result[i])[0])
-	user_list.append(u2)
+      u2=str((result[i])[0])
+      user_list.append(u2)
    if u1 in user_list:
-          values1 = mycursor.execute("SELECT password FROM customer_details WHERE customer_id=%s",[u1])
-   	  result1 =mycursor.fetchone()
-   	  r=result1[0]
-	  if r==p:
-	    val=json.dumps({'status':True})
-          else:
-	    val=json.dumps({'status':False})
+      values1 = mycursor.execute("SELECT password FROM customer_details WHERE customer_id=%s",[u1])
+      result1 =mycursor.fetchone()
+      r=result1[0]
+      if r==p:
+         val=json.dumps({'status':True})
+      else:
+         val=json.dumps({'status':False})
    else:
-	 val=json.dumps({'status':False})
+      val=json.dumps({'status':False})
    dba.close()
    return val
 
-@app.route('/loca/')
-def lo():
-    k={'tyt':'swa','lus':'sha'}
-    l=json.dumps(k)
-    return l
 
 @app.route('/location',methods = ['POST', 'GET'])
 def locate():
    if request.method == 'GET':
       customer_id = request.args.get('c_id')
       app_id = request.args.get('a_id')
-      dba = MySQLdb.connect(host="localhost", user="root", passwd="Swathi_123", db=customer_id)
+      dba = connection = pymysql.connect(host='localhost',user='root', password='Swathi_123',db=customer_id)
       mycursor = dba.cursor()
       values = mycursor.execute("SELECT latitude, longitude FROM tracking_details WHERE app_id=%s ORDER BY id DESC LIMIT 1",[app_id])
       result =mycursor.fetchall()
@@ -59,6 +56,13 @@ def locate():
       l2=json.dumps(l1)
       dba.close()
    return l2
+
+#to send json data
+"""@app.route('/loca/')
+def lo():
+    k={'tyt':'swa','lus':'sha'}
+    l=json.dumps(k)
+    return l"""
       
 
 if __name__ == '__main__':
