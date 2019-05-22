928f54bb19d48b463ed016a864dcbce07626b0ab
swathi5896 <swathi5896.r@gmail.com>
2019-05-22 16:22:10 +0530

    devices count

--- bse_fleet.py
+++ bse_fleet.py
@@ -34,16 +34,35 @@ def authorize():
 
 @app.route('/location', methods=['POST', 'GET'])
 def locate():
-    if request.method == 'GET':
-        customer_id = request.args.get('c_id')
-        app_id = request.args.get('a_id')
+    if request.method == 'POST':
+        b = request.get_json()
+        customer_id = b['c_id']
+        app_id = b['a_id']
         myclient=pymongo.MongoClient("mongodb://localhost:27017/")
         mydb=myclient[customer_id]
         mycol=mydb["tracking_details"]
-        location=mycol.find_one({'app_id':app_id},{"_id":False})
+        location=mycol.find_one({'app_id':app_id},{"_id":False,"speed":False,"datetime":False,"app_id":False})
         myclient.close()
         l=json.dumps(location)
     return l
+
+@app.route('/devices', methods=['POST', 'GET'])
+def devices_count():
+    if request.method == 'POST':
+        cus_id = request.get_json()
+        customer_id = cus_id['c_id']
+        dba = pymysql.connect(host='localhost', user='root',password='Swathi_123', db='admin')
+        mycursor = dba.cursor()
+        mycursor.execute("SELECT app_id FROM app_details WHERE customer_id=%s", [customer_id])
+        result = mycursor.fetchall()
+        print (result)
+        devs=[]
+        for i in result:
+            devs.append(i[0])
+        d={"devices":devs,"nod":len(result)}    
+        d=json.dumps(d)
+        dba.close()
+    return d    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
